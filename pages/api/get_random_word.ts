62e243665fdf17ddabf79d899f7b778f411f5aeb
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-04-01 23:52:32 +0800

    backend: feat: POST req to backend (with game settings num_chars and diff) WORKS

--- pages/api/get_random_word.ts
+++ pages/api/get_random_word.ts
@@ -1,12 +1,43 @@
 import EnglishDictSingleton from '@/services/english_dictionary'
+import { IGameSettings } from '@/store/game_settings'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 interface Data {
     random_word: {}
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    const word = (await EnglishDictSingleton).get_random_word()
+// index signature
+const diff_to_freq_map: { [key: string]: number } = {
+    easy: 5,
+    medium: 0.1,
+    hard: 0.01,
+    very_hard: 0.001,
+}
 
-    res.status(200).json({ random_word: word })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    if (req.method !== 'POST') {
+        res.status(400)
+        throw new Error('should be a post request')
+    }
+    const { num_chars, difficulty }: IGameSettings = req.body
+    const diff = diff_to_freq_map[difficulty]
+
+    let random_word: string
+    let frequency: number
+
+    const EngDict = await EnglishDictSingleton
+    while (true) {
+        console.log('---- fetching random word ----')
+        random_word = EngDict.get_random_word()
+        frequency = await EngDict.get_word_frequency(random_word)
+        if (
+            random_word.length === num_chars &&
+            frequency >= diff &&
+            /^[a-zA-Z]*?$/.test(random_word)
+        ) {
+            console.log(`***** THE FINAL WORD: ${random_word} with a freq of ${frequency}`)
+            res.status(200).json({ random_word: random_word })
+            return
+        }
+    }
 }

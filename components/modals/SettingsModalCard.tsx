0174318b386c7a4446af8f3620402f1fba4f4e46
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-04-03 12:25:23 +0800

    FE: cleanup: no need for form validation on settings modal

--- components/modals/SettingsModalCard.tsx
+++ components/modals/SettingsModalCard.tsx
@@ -1,45 +1,19 @@
 import useGameSettings$ from '@/store/game_settings'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { FieldValues, useForm } from 'react-hook-form'
 import { AiFillSignal } from 'react-icons/ai'
 import { SiLetterboxd } from 'react-icons/si'
-import { z } from 'zod'
-
-const schema = z.object({
-    min_chars: z.number().min(5).max(8),
-    difficulty: z.string(),
-})
-
-type FormData = z.infer<typeof schema>
 
 interface Props {
     onCloseSettingsModal: () => void
 }
 
 export default function SettingsModalContainer({ onCloseSettingsModal }: Props) {
-    // NOTE: minChars is only used for display, the actual min chars to transfer to the store
-    // is inside onFormSubmitted's data
-    // const [minChars, setMinChars] = useState(5)
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid },
-    } = useForm<FormData>({ resolver: zodResolver(schema) })
-
     const gameSettings$ = useGameSettings$((state) => state.gameSettings$)
     const changeNumChars$ = useGameSettings$((state) => state.changeNumChars$)
     const changeDifficulty$ = useGameSettings$((state) => state.changeDifficulty$)
     const resetGameSettings$ = useGameSettings$((state) => state.resetGameSettings$)
 
-    function onFormSubmitted(data: FieldValues) {
-        console.log(data)
-    }
-
     return (
-        <form
-            onSubmit={handleSubmit(onFormSubmitted)}
-            className="flex flex-col items-center justify-center h-80 w-[28rem] bg-white rounded-3xl p-10"
-        >
+        <form className="flex flex-col items-center justify-center h-80 w-[28rem] bg-white rounded-3xl p-10">
             <div className="mb-4 mt-3 flex items-center w-full pb-2 border-b pt-2 border-t">
                 <span className="w-8">
                     <SiLetterboxd />
@@ -48,11 +22,8 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
                     Minimum Chars
                 </label>
                 <input
-                    {...register('min_chars', { valueAsNumber: true })}
                     value={gameSettings$.num_chars}
-                    // value={minChars}
                     onChange={(e) => changeNumChars$(parseInt(e.target.value))}
-                    // onChange={(e) => setMinChars(parseInt(e.target.value))}
                     id="min_chars"
                     type="range"
                     min={5}
@@ -73,13 +44,12 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
                     Difficulty
                 </label>
                 <select
-                    {...register('difficulty')}
                     id="difficulty"
+                    name="difficulty"
                     className="ml-auto p-2 border-none rounded-md outline-none"
                     value={gameSettings$.difficulty}
                     onChange={(e) => changeDifficulty$(e.target.value)}
                 >
-                    <option selected>Medium</option>
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>

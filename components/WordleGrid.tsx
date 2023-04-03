5f0e275bca74326f99e500a9fbe9925f566430f4
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-04-03 21:55:33 +0800

    FE: feat: basic inputs working, guess tracker stores basic functionality, details:
    typing works, deleting a char works, ENTER works
    
    no input validation yet, no validation if guess is valid on ENTER

--- components/WordleGrid.tsx
+++ components/WordleGrid.tsx
@@ -1,5 +1,9 @@
 import { Space_Grotesk } from 'next/font/google'
 import { useEffect, useState } from 'react'
+import useRandomWordStore$ from '@/store/random_word'
+import useGameSettings$ from '@/store/game_settings'
+import useGuessTracker$ from '@/store/wordle_guess'
+import WordleRow from './WordleRow'
 
 const spaceGrotesk = Space_Grotesk({
     subsets: ['latin'],
@@ -7,42 +11,50 @@ const spaceGrotesk = Space_Grotesk({
 })
 
 export default function WordleGrid() {
-    const [currentGuess, setCurrentGuess] = useState<string[]>(Array(5).fill(''))
-    console.log(currentGuess)
+    const currentRandomWord$ = useRandomWordStore$((state) => state.currentRandomWord$)
+    const clearCurrentWord$ = useRandomWordStore$((state) => state.clearCurrentWord$)
+    const renewCurrentWord$ = useRandomWordStore$((state) => state.renewCurrentWord$)
+    const gameSettings$ = useGameSettings$((state) => state.gameSettings$)
+
+    const setCurrentRandomWord$ = useGuessTracker$((state) => state.setCurrentRandomWord$)
+    const allGuesses$ = useGuessTracker$((state) => state.allGuesses$)
+    const addLetterToGuess$ = useGuessTracker$((state) => state.addLetterToGuess$)
+    const removeLastLetterFromGuess$ = useGuessTracker$(
+        (state) => state.removeLastLetterFromGuess$,
+    )
+    const incrementRow$ = useGuessTracker$((state) => state.incrementRow$)
 
     useEffect(() => {
         function onKeyUp(e: KeyboardEvent) {
             e.preventDefault()
             const key = e.key
 
-            if (/^[a-zA-Z]$/.test(e.key)) {
-                setCurrentGuess((state) => [...state, e.key])
-                console.log(currentGuess)
+            if (/^[a-zA-Z]$/.test(key)) {
+                addLetterToGuess$(key)
             } else if (key === 'Backspace' || key === '{bksp}') {
-                console.log('bksp')
+                removeLastLetterFromGuess$()
             } else if (key === 'Enter' || key === '{enter}') {
-                console.log('Enter')
+                incrementRow$()
             }
         }
         window.addEventListener('keyup', onKeyUp)
 
         return () => window.removeEventListener('keyup', onKeyUp)
-    }, [currentGuess])
+    }, [])
+
+    useEffect(() => {
+        renewCurrentWord$(gameSettings$)
+        setCurrentRandomWord$(currentRandomWord$)
+    }, [])
 
     return (
         // words container
         <div
             className={`flex flex-col justify-between items-center bg-custom-red h-[58%] w-[60%] py-12 rounded-3xl text-[1.3rem] ${spaceGrotesk.className}`}
         >
-            {/* word */}
-            {currentGuess.map((_, idx) => (
-                <div className="wordle-word" key={idx}>
-                    <div className="wordle-letter"></div>
-                    <div className="wordle-letter"></div>
-                    <div className="wordle-letter"></div>
-                    <div className="wordle-letter"></div>
-                    <div className="wordle-letter"></div>
-                </div>
+            current rand word: {currentRandomWord$ && currentRandomWord$}
+            {allGuesses$.map((wordGuess, idx) => (
+                <WordleRow word={wordGuess} key={idx} />
             ))}
         </div>
     )
--- store/wordle_guess.ts
+++ store/wordle_guess.ts
@@ -1,7 +1,8 @@
+import { v4 as uuidv4 } from 'uuid'
 import { create } from 'zustand'
-import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+import middleware from './zustand_middleware'
 
-interface IGuessState {
+export interface LetterGuess {
     id: string
     letter: string
     isBlank: boolean
@@ -9,6 +10,93 @@ interface IGuessState {
     isLetterInCorrectPosition: boolean
 }
 
-const guessStore = (set: any) => ({
-    // dsadas
+interface IGuessStore {
+    currentRandomWord$: string
+    setCurrentRandomWord$: (curWord: string) => void
+    allGuesses$: LetterGuess[][]
+    currentRowIdx$: number
+    currentRow$: LetterGuess[]
+    addLetterToGuess$: (letterToAdd: string) => void
+    removeLastLetterFromGuess$: () => void
+    incrementRow$: () => void
+}
+
+// referencing state variables inside state functions: https://github.com/TePMo-Tapo4eK/Tic-Tac-Toe/blob/main/src/store/index.tsx
+// settings stores before returning: https://github.com/nexxeln/nexdle/blob/main/src/store.ts
+// computed properties: https://github.com/pmndrs/zustand/issues/132#issuecomment-669619250
+const guessStore = (set: any, get: any) => ({
+    currentRandomWord$: '',
+
+    setCurrentRandomWord$: (curWord: string) => {
+        set((state: IGuessStore) => {
+            state.currentRandomWord$ = curWord
+        })
+    },
+
+    allGuesses$: generateEmptyGuessArray(5),
+
+    currentRowIdx$: 0,
+
+    incrementRow$: () =>
+        set((state: IGuessStore) => {
+            state.currentRowIdx$++
+        }),
+
+    currentRow$: () => get().allGuesses$[get().currentRowIdx$],
+
+    addLetterToGuess$: (letterToAdd: string) => {
+        const current = (): LetterGuess[] => get().currentRow$()
+        const currentRow: LetterGuess[] = JSON.parse(JSON.stringify(current()))
+        const nextBlankCell = currentRow.find((letter) => letter.isBlank) as LetterGuess
+
+        if (!nextBlankCell) return
+        nextBlankCell.letter = letterToAdd
+        nextBlankCell.isBlank = false
+
+        set((state: IGuessStore) => {
+            const newState: LetterGuess[][] = JSON.parse(JSON.stringify(state.allGuesses$))
+            newState.splice(state.currentRowIdx$, 1, currentRow)
+            state.allGuesses$ = newState
+        })
+    },
+
+    removeLastLetterFromGuess$: () => {
+        const current = (): LetterGuess[] => get().currentRow$()
+        const currentRow: LetterGuess[] = JSON.parse(JSON.stringify(current()))
+        const itemToRemove = [...currentRow]
+            .reverse()
+            .find((letter) => !letter.isBlank) as LetterGuess
+        if (!itemToRemove) return
+        itemToRemove.letter = ''
+        itemToRemove.isBlank = true
+        itemToRemove.isLetterInWord = false
+        itemToRemove.isLetterInCorrectPosition = false
+
+        set((state: IGuessStore) => {
+            const newState: LetterGuess[][] = JSON.parse(JSON.stringify(state.allGuesses$))
+            newState.splice(state.currentRowIdx$, 1, currentRow)
+            state.allGuesses$ = newState
+        })
+    },
 })
+
+const useGuessStore = create<IGuessStore>()(middleware(guessStore))
+export default useGuessStore
+
+function generateEmptyGuessArray(wordSize: number): LetterGuess[][] {
+    const finalArr: LetterGuess[][] = []
+    for (let i = 0; i < 6; i++) {
+        const nestedArr: LetterGuess[] = []
+        for (let i = 0; i < wordSize; i++) {
+            nestedArr.push({
+                id: uuidv4(),
+                letter: '',
+                isBlank: true,
+                isLetterInWord: false,
+                isLetterInCorrectPosition: false,
+            })
+        }
+        finalArr.push(nestedArr)
+    }
+    return finalArr
+}

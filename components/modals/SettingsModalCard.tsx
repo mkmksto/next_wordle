97d996f2999df855e30ae7322c0cfae943ae8832
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-04-06 15:19:48 +0800

    FE: settins modal: implement useResetGame on click 'save settings and restart'

--- components/modals/SettingsModalCard.tsx
+++ components/modals/SettingsModalCard.tsx
@@ -1,3 +1,4 @@
+import useResetGame from '@/hooks/useResetGame'
 import useGameSettings$ from '@/store/game_settings'
 import { AiFillSignal } from 'react-icons/ai'
 import { SiLetterboxd } from 'react-icons/si'
@@ -12,8 +13,10 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
     const changeDifficulty$ = useGameSettings$((state) => state.changeDifficulty$)
     const resetGameSettings$ = useGameSettings$((state) => state.resetGameSettings$)
 
+    const { handleReset } = useResetGame()
+
     return (
-        <form className="flex flex-col items-center justify-center h-80 w-[28rem] bg-white rounded-3xl p-10">
+        <form className="flex flex-col items-center justify-center h-80 min-w-[28rem] max-w-fit bg-white rounded-3xl p-10">
             <div className="mb-4 mt-3 flex items-center w-full pb-2 border-b pt-2 border-t">
                 <span className="w-8">
                     <SiLetterboxd />
@@ -28,7 +31,7 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
                     type="range"
                     min={5}
                     max={8}
-                    className="ml-auto outline-none"
+                    className="ml-auto outline-none accent-orange-400 bg-neutral-100"
                 />
                 <span className="ml-2">{gameSettings$.num_chars}</span>
             </div>
@@ -46,7 +49,7 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
                 <select
                     id="difficulty"
                     name="difficulty"
-                    className="ml-auto p-2 border-none rounded-md outline-none"
+                    className="ml-auto p-2 border-none rounded-full"
                     value={gameSettings$.difficulty}
                     onChange={(e) => changeDifficulty$(e.target.value)}
                 >
@@ -57,11 +60,11 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
                 </select>
             </div>
 
-            <div className="mt-auto w-full flex justify-between">
+            <div className="mt-auto w-full flex justify-between gap-x-3">
                 <button
                     onClick={() => onCloseSettingsModal()}
                     type="submit"
-                    className="settings-buttons border-red-400"
+                    className="settings-buttons border-orange-300"
                 >
                     Close
                 </button>
@@ -70,12 +73,15 @@ export default function SettingsModalContainer({ onCloseSettingsModal }: Props) 
                     type="reset"
                     className="settings-buttons"
                 >
-                    Reset
+                    Reset Settings
                 </button>
 
                 {/* Todo: Implement */}
                 <button
-                    onClick={() => console.log('save and restart')}
+                    onClick={() => {
+                        onCloseSettingsModal()
+                        handleReset()
+                    }}
                     type="button"
                     className="settings-buttons"
                 >

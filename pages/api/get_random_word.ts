6fbb686c5bb4a024a0963d2917394b0f419f2218
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-04-15 10:16:22 +0800

    BE: feat: (basic) setting the random word from the backend instead

--- pages/api/get_random_word.ts
+++ pages/api/get_random_word.ts
@@ -1,6 +1,9 @@
+import { createSpClient } from '@/lib/supabaseClient'
 import EnglishDictSingleton from '@/services/english_dictionary'
 import { IGameSettings } from '@/store/game_settings'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './auth/[...nextauth]'
 
 interface Data {
     random_word: {}
@@ -25,6 +28,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     let random_word: string
     let frequency: number
 
+    const session = await getServerSession(req, res, authOptions)
+    const { supabaseAccessToken } = session ?? {}
+    // console.log(`supabase access token: ${supabaseAccessToken}`)
+    // console.log(`user id: ${session?.user.id}`)
+    const supabase = createSpClient(supabaseAccessToken ?? '')
+
     const EngDict = await EnglishDictSingleton
     while (true) {
         console.log('---- fetching random word ----')
@@ -36,6 +45,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
             /^[a-zA-Z]*?$/.test(random_word)
         ) {
             console.log(`***** THE FINAL WORD: ${random_word} with a freq of ${frequency}`)
+
+            // update supabase DB
+            const { data, error } = await supabase
+                .from('current_word')
+                .update({ secret_word: random_word })
+                .eq('user_id', session?.user.id)
+            // .upsert({ user_id: session?.user.id, secret_word: random_word })
+            if (error) {
+                console.error('failed to update supabase DB with new secret word')
+                console.error(error)
+            } else {
+                console.log('successfully updated supabsae DB')
+            }
+
             res.status(200).json({ random_word: random_word })
             return
         }
